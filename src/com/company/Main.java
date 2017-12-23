46c06bba81b7bcd9af28954ab2824c273e17f272
hsalgi100 <helen.salgi@gmail.com>
2017-12-23 17:46:21 -0600

    No longer outputs variables assigned to zero.
    Signed-off-by: hsalgi100 <helen.salgi@gmail.com>

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,18 +2,20 @@ package com.company;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 import java.util.StringJoiner;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-        File f = new File("C:/Users/helen.DESKTOP-9PHR09U/Desktop/JProjects/p2-parser/Test1.txt");
+        File f = new File("C:/Users/helen.DESKTOP-9PHR09U/Desktop/JProjects/p2-parser/Test2.txt");
 	    Scanner input = new Scanner(f);
 	    while(input.hasNextLine()){
 	        String[] parts = input.nextLine().split("=");
+	        String numPart = parts[parts.length - 1];
+	        if(numPart.equals(" 0")){
+	            continue;
+            }
             StringJoiner joiner = new StringJoiner(":");
             for(String part: parts){
                 joiner.add(part);
