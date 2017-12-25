1876f0f185a7e01df7909dc8a4e00d15687df54b
hsalgi100 <helen.salgi@gmail.com>
2017-12-24 19:07:18 -0600

    Fixed not printing zero and added the thing where it goes through duplicates and only prints the last one.
    Signed-off-by: hsalgi100 <helen.salgi@gmail.com>

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,25 +3,41 @@ package com.company;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.StringJoiner;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
-        File f = new File("C:/Users/helen.DESKTOP-9PHR09U/Desktop/JProjects/p2-parser/Test2.txt");
-	    Scanner input = new Scanner(f);
-	    while(input.hasNextLine()){
-	        String[] parts = input.nextLine().split("=");
-	        String numPart = parts[parts.length - 1];
-	        if(numPart.equals(" 0")){
-	            continue;
-            }
-            StringJoiner joiner = new StringJoiner(":");
-            for(String part: parts){
-                joiner.add(part);
-            }
-            String output = joiner.toString();
-            System.out.println(output);
+    public static void main(String[] args) {
+        File f = new File("C:/Users/helen.DESKTOP-9PHR09U/Desktop/JProjects/p2-parser/Test3.txt");
+        Scanner input = null;
+	    try{
+            input = new Scanner(f);
+        }catch(FileNotFoundException fileNotFound){
+            System.out.println("This file does not exist.");
+            System.exit(1);
         }
+	    int aValue = 0;
+	    int bValue = 0;
+
+	    while(input.hasNextLine()){
+	        String[] parts = input.nextLine().split(" = ");
+	        String numPart = parts[parts.length - 1];
+
+	        if(parts[0].equals("a")){
+                int value = Integer.parseInt(numPart);
+                aValue = value;
+            }
+            if(parts[0].equals("b")){
+                int value = Integer.parseInt(numPart);
+                bValue = value;
+            }
+        }
+        if(aValue == 0){
+            System.out.print("");
+        }
+        else{System.out.println("a: " + aValue);}
+        if(bValue == 0){
+            System.out.print("");
+        }
+        else{System.out.println("b: " + bValue);}
     }
 }
